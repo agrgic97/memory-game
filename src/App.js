3e7746d8a7bb8d326c7deecbd1317ad35678346c
agrgic97 <grgic.antonio97@gmail.com>
2022-03-20 00:18:46 +0100

    added first characters

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Header from './components/Header';
+import characters from './characters.json';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      characters
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(id) {
+    console.log(id);
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header></Header>
+        {this.state.characters.map(character => {
+          return <img onClick={this.handleClick.bind(this, character.id)} key={character.id} src={character.image} alt={character.name} width="250" height="320"></img>
+        })}
       </div>
     );
   }
+  
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+    render() {
+        return(
+            <div className="Header">One Piece Memory Game</div>
+        )
+    }
+}
+
+export default Header;
